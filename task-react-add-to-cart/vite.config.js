10e4513e15e3d03a00727a4fb8d4767465682e80
Sharath M <sharathm868@gmail.com>
2026-08-27 13:42:24 +0530

    Deploy React add to cart task

--- task-react-add-to-cart/vite.config.js
+++ task-react-add-to-cart/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ultimez-bootstrap4-list-groups/task-react-add-to-cart/',
+  build: {
+    outDir: '.',
+    emptyOutDir: false
+  }
 })
